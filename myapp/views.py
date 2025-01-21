c3c9ea6958b31616b7c3b15e508bd14ce9216e9b
artyom <batmanov.2016@mail.ru>
2025-01-22 00:18:43 +0300

    add PUT and PATCH for UserProfileView

--- myapp/views.py
+++ myapp/views.py
@@ -40,6 +40,20 @@ class UserProfileView(APIView):
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
+    def patch(self, request):
+        serializer = UserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request):
+        serializer = UserSerializer(request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class OrganizationView(APIView):
     def get(self, request):
