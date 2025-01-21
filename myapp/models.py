34b9c9b636f03ed7551f3d3fba8114cdf653a8f8
artyom <batmanov.2016@mail.ru>
2025-01-22 00:03:08 +0300

    добавлена возможность добавить аватарку + автоматическое переименование картинки и уменьшения картинки до максимального размера 200x200px

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,21 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from PIL import Image
+import os
+import uuid
+from django.utils.text import slugify
+from django.core.files.storage import default_storage
+import io
+from django.core.files.base import ContentFile
+
+
+
+
+
+def avatar_upload_to(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{slugify(instance.first_name)}_{uuid.uuid4().hex}.{ext}"
+    return os.path.join('avatars/', filename)
 
 
 class UserManager(BaseUserManager):
@@ -19,6 +35,8 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
+    avatar = models.ImageField(upload_to=avatar_upload_to, blank=True, null=True)
+
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -39,12 +57,35 @@ class User(AbstractBaseUser):
         return self.email
 
     def has_perm(self, perm, obj=None):
-        """Does the user have a specific permission?"""
         return self.is_superuser
 
     def has_module_perms(self, app_label):
-        """Does the user have permissions to view the app `app_label`?"""
         return self.is_superuser
+
+
+    def save(self, *args, **kwargs):
+        if self.avatar and not self.avatar.name.startswith('avatars/'):
+            old_avatar = self.avatar.name  # Сохраняем старое имя для удаления
+            self.avatar.name = avatar_upload_to(self, self.avatar.name)
+
+            # Удаляем старый файл, если он существует
+            if old_avatar and default_storage.exists(old_avatar):
+                default_storage.delete(old_avatar)
+
+            # Открываем изображение и изменяем размер
+            img = Image.open(self.avatar)
+            img.thumbnail((200, 200))  # Изменение размера до 200x200
+
+            # Сохраняем измененное изображение в памяти
+            output = io.BytesIO()
+            img_format = 'JPEG' if img.format == 'JPEG' else 'PNG'
+            img.save(output, format=img_format)
+            output.seek(0)
+
+            # Заменяем оригинальный файл новым уменьшенным
+            self.avatar = ContentFile(output.read(), name=self.avatar.name)
+
+        super().save(*args, **kwargs)
 
 
 class Organization(models.Model):
